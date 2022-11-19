804b1405ca116fa3cddc4d6bdc9e365d4177a8b9
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-11-19 19:03:54 -0300

    montagem da base

--- jogo_Forca.py
+++ jogo_Forca.py
@@ -1,16 +1,85 @@
+import os
 
-
-palavra = 'dado'
+palavra = 'capricornio'
 acertos = 0
 erros = 0
 tracos = '_ ' * len(palavra)
+letras_acertadas = ''
+letras_erradas = ''
+
+forca = """
+----|
+    |    
+    """
+
+nada = """
+
+
+"""
+
+cabeca = """
+    O
+"""
+corpo = """
+    O
+    |
+"""
+braco_esq = """
+    O
+    |\\
+"""
+braco_dir = """
+    O
+   /|\\
+"""
+perna_esq = """
+    O
+   /|\\
+     \\
+"""
+perna_dir = """
+    O
+   /|\\
+   / \\
+"""
+
+
+chances = [
+    nada,
+    cabeca,
+    corpo,
+    braco_esq,
+    braco_dir,
+    perna_esq,
+    perna_dir,
+]
+
 
 while acertos != len(palavra) and erros < 7:
-    letra = str(input('Digite uma letra: '))
 
+    mostrar_letra = ''
+    for letra  in palavra:
+        os.system("clear")
+        if letra in letras_acertadas:
+            mostrar_letra += letra + ' '
+        else:
+            mostrar_letra += '_ '
+    
+    print(f'A palavra contém: {len(palavra)} Letras')
+    print(mostrar_letra)
+    print(forca+chances[erros])
+    print(f'Letras Erradas: {letras_erradas}')
+
+
+    letra = input('Digite uma letra: ')
+    
     if letra in palavra:
-        acertos += 1
-        print(tracos)
+        os.system("clear")
+        letras_acertadas += letra + ' '
+        acertos += 1        
     else:
+        letras_erradas += letra + ' '
         erros += 1
-        print(f'Erros: {erros}')
+        print('Você Perdeu.')
+
+print('Você venceu!')
